41d5549f3733d06682860eafbb881e186fa28c41
ChidiChuks <chukschiboy@gmail.com>
2020-04-14 16:39:44 +0100

    playing around with containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+// personal imports
+import './ui/home.dart';
+
 void main() {
   runApp(
     new MaterialApp(
       title: "Simple Layouts",
       home: new Home(),
     ),
-  )
+  );
 }
