6d6b3b86c272fb886b04babc3295f0f97079e86b
ChidiChuks <chukschiboy@gmail.com>
2020-04-16 09:24:45 +0100

    creating an image asset

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,19 +6,80 @@ class Home extends StatelessWidget {
     return Center(
       child: Container(
         alignment: Alignment.center,
+        padding: EdgeInsets.all(30.0),
         color: Colors.deepOrangeAccent,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Margherita",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontFamily: 'Oxygen',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Tomato, Mozzarella, Basil",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontFamily: 'Oxygen',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            
+            Divider(
+              height: 20.0,
+              color: Colors.white70,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Marinara",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontFamily: 'Oxygen',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Tomato, Garlic",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontFamily: 'Oxygen',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
         /* width: 192.0,
         height: 96.0, 
         margin: EdgeInsets.only(left:50.0), */
-        child: Text(
-          "Pizza",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 80.0,
-            decoration: TextDecoration.none,
-            color: Colors.white,
-          ),
-        ),
       ),
     );
   }
