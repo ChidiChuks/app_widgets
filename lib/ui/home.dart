2eac3cfbd8591fea1edf6faedcf86a735dbd72c4
ChidiChuks <chukschiboy@gmail.com>
2020-04-16 10:40:27 +0100

    creating a reaised button with an alert

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -40,7 +40,6 @@ class Home extends StatelessWidget {
                 ),
               ],
             ),
-            
             Divider(
               height: 20.0,
               color: Colors.white70,
@@ -75,12 +74,57 @@ class Home extends StatelessWidget {
                 ),
               ],
             ),
+            PizzaImageWidget(),
+            OrderButton(),
           ],
         ),
-        /* width: 192.0,
-        height: 96.0, 
-        margin: EdgeInsets.only(left:50.0), */
       ),
     );
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset =
+        AssetImage('images/dancing-gifs-transparent-20.png');
+    Image image = Image(
+      image: pizzaAsset,
+      width: 400.0,
+      height: 400.0,
+    );
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order your Pizza!"),
+        color: Colors.yellow[400],
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
   }
 }
